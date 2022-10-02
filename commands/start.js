87eab14dae4fcccfdab7720816520a16d9ae83c3
Michael DiPaolo <midipaolo@thesalisburyschool.org>
2022-10-02 11:01:53 -0400

    Remove new-character.js command Replace with start.js command

---
+++ commands/start.js
@@ -0,0 +1,81 @@
+const { SlashCommandBuilder } = require('discord.js');
+const Profile = require('../db/schema/profile');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('start')
+		.setDescription('Create a character and start your journey!')
+		.addStringOption((option) =>
+			option
+				.setName('name')
+				.setDescription('Your character\'s name?')
+				.setRequired(true),
+		)
+		.addStringOption((option) =>
+			option
+				.setName('faction')
+				.setDescription('Choose your faction!')
+				.setRequired(true)
+				.addChoices(
+					{ name: 'Faction 1', value: 'Faction 1' },
+					{ name: 'Faction 2', value: 'Faction 2' },
+				),
+		)
+		.addStringOption((option) =>
+			option
+				.setName('color')
+				.setDescription('Choose your favorite color!')
+				.setRequired(true)
+				.addChoices(
+					{ name: 'Red', value: 'red' },
+					{ name: 'Blue', value: 'blue' },
+					{ name: 'Yellow', value: 'yellow' },
+				),
+		)
+		.addStringOption((option) =>
+			option
+				.setName('class')
+				.setDescription('Choose your class!')
+				.setRequired(true)
+				.addChoices(
+					{ name: 'Warrior', value: 'warrior' },
+					{ name: 'Mage', value: 'mage' },
+					{ name: 'Rogue', value: 'rogue' },
+				),
+		),
+
+	async execute(interaction) {
+		const name = interaction.options.getString('name');
+		const faction = interaction.options.getString('faction');
+		const fcolor = interaction.options.getString('color');
+		const cclass = interaction.options.getString('class');
+		
+		const intGuild = interaction.guild;
+		const intGuildId = interaction.guild.id;
+		
+		const intUserId = interaction.user.id;
+
+		const newProfile = new Profile({
+			discordId: intUserId,
+			discordName: interaction.user.name,
+
+			guild: intGuild,
+			guildId: intGuildId,
+
+			character: name,
+			faction: faction,
+			fcolor: fcolor,
+			cclass: cclass,
+		});
+
+		await newProfile.save();
+
+		await interaction.reply(
+			`Yes! That's it! You're **${name}**, the **${cclass}** from **${faction}**!!
+			\nBut.. I never knew your favorite color was **${fcolor}**...
+			\nNo matter! You're here now! Welcome, **${name}**! And good luck!
+			\n
+			\nTesting DB: ${newProfile}`,
+		);
+	},
+};
