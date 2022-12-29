9bb6eea74e9fa51b08f8769fc3b4ddc0207658ec
Michael DiPaolo <midipaolo@thesalisburyschool.org>
2022-12-28 20:05:35 -0500

    Move models to their own folder

--- db/models/Quiz.js
+++ db/models/Quiz.js
@@ -2,14 +2,23 @@ const { model, Schema } = require('mongoose');
 const ObjectId = Schema.Types.ObjectId;
 
 const quizSchema = new Schema({
-	_id: ObjectId,
 
 	name: String,
+	abbrv: String,
 	length: Number,
+	linkedin: Boolean,
 
 	questions: [{
 		ques_id: ObjectId,
-		descr: String,
+		qNum: Number,
+		text: String,
+		qtype: String,
+		choices: {
+			a: String,
+			b: String,
+			c: String,
+			d: String,
+		},
 		answer: String,
 
 		tot_attempts: Number,
@@ -17,10 +26,11 @@ const quizSchema = new Schema({
 		avg_sec: Number,
 		med_sec: Number,
 		mass_ef: { type: Number, default: 2.5 },
-	}],
+	},
+	],
 
 	avg_ef: Number,
 	med_ef: Number,
 });
 
-module.exports = model('Student', quizSchema, 'students');
+module.exports = model('Quiz', quizSchema, 'quizzes');
