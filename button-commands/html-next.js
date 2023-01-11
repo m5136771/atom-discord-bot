54f6f9d43e2e5bc8a49462c5c71a5e285b6c540a
Michael DiPaolo <midipaolo@thesalisburyschool.org>
2023-01-11 09:17:11 -0500

    Add console logs for debugging

--- button-commands/html-next.js
+++ button-commands/html-next.js
@@ -1,8 +1,6 @@
-/* eslint-diQuestionble no-inline-comments */
 /*
 
 	Button Command triggers when selecting quiz name from Init cmd && when continuing practice questions
-	[ ] Log response to db
 
 */
 
@@ -10,7 +8,7 @@ const { EmbedBuilder, ComponentType } = require('discord.js');
 const Attempt = require('../db/models/Attempt');
 const Question = require('../db/models/Question');
 const Student = require('../db/models/Student');
-const { getRandomInt, easinessCalc, efCalc, daysToNext, newDate, lastEf, docSave } = require('../helpers/misc');
+const { getRandomInt, easinessCalc, efCalc, daysToNext, newDate, docSave } = require('../helpers/misc');
 
 const { ansRow, contRow } = require('../assets/action-rows');
 
@@ -21,39 +19,39 @@ module.exports = {
 	async execute(interaction) {
 
 		// Log Start Time
-		const today = new Date();
-		const startTime = today;
+		const startTime = new Date();
+		const startTimeISO = startTime.toISOString();
 		console.log(`Start time: ${startTime}`);
+		console.log(`startTimeISO: ${startTimeISO}`);
 
 		// Find Student in DB
 		const student = await Student.findOne({
 			disc_id: interaction.user.id,
 		}).exec();
-		console.log(`Student: ${student.name}\nStudent ID: ${student._id}`);
+		console.log(`Student: ${student.disc_tag}\nStudent ID: ${student._id}`);
 		const studentId = student._id;
 
 		let nextQuestion = undefined;
 
-		// 1. Pull questions, if any, from "next_up"
-		// next_up info is logged into Attempt, which links to student and question
-		// 1. find oldest Attempt doc next_up today linked to this Student
-		await Attempt
-			.findOne()
-			.where('next_up', today)
+		// 1. Pull questions, if any, from "next_up" (next_up info is logged into Attempt Docs, which links to student and question)
+		// IF 'reattempted: false', meaning it has only displayed once since creation
+		// and set the one found to 'reattempted: true' so we know this attempt has been sent a second time.
+		const nextUp = await Attempt
+			.findOneAndUpdate({ r_atmp: true })
+			.where('next_up').lte(startTimeISO)
 			.where('student', studentId)
-			.sort({ ['updatedAt:']: 1 })
+			.where('r_atmp', false)
+			.sort({ next_up: -1 })
 			// sort by 'first in; first out'
 
 			.then(
 				(doc) => {
 					if (doc === null) {
 						console.log('Nothing to see here...');
-						return;
 					} else {
-						const questionDue = Question
-							.findById(doc.qs._id);
-						console.log(`questionDue is: ${questionDue}`);
-						nextQuestion = doc;
+						nextQuestion = doc.qs._id;
+						console.log(`Attempt doc found for next_up.lte(today): ${nextQuestion}\nnextUp = Mongo Doc`);
+						return doc;
 					}
 				},
 				(err) => {
@@ -61,7 +59,18 @@ module.exports = {
 				},
 			).catch(console.error);
 
-		// 2. IF !next_up, Pull from DB
+		if (!nextUp) {
+			console.log(`nextUp: ${nextUp}...`);
+		} else {
+			const questionDue = await Question
+				.findOneAndUpdate({ $inc: { tot_atmp: 1 } })
+				.where('_id', nextQuestion);
+			console.log(`questionDue is: ${questionDue}`);
+			nextQuestion = questionDue;
+			console.log(`Next Question found in Next_up: ${nextQuestion}`);
+		}
+
+		// 2. IF !nextQuestion yet, Pull from DB:
 		if (!nextQuestion) {
 			console.log('Querying DB for count...');
 			const queryForCount = await Question
@@ -69,8 +78,18 @@ module.exports = {
 				.where('tags').in(['html'])
 				.where('atmp_by').ne(studentId);
 
+			console.log(`queryForCount = ${queryForCount}`);
+
+			if (queryForCount === 0) {
+				await interaction.update(
+					{ content: 'You\'re insane!! You answered every question at least once!!!', ephemeral: true, embeds: [], components: [] },
+				);
+				console.log('No more quiz questions.. ending quiz.');
+				return;
+			}
+
 			// grab a question at random
-			const ranNum = getRandomInt(1, queryForCount);
+			const ranNum = getRandomInt(0, queryForCount - 1);
 			console.log(`Random number: ${ranNum} out of ${queryForCount}`);
 
 			console.log('Querying DB for question...');
@@ -80,18 +99,15 @@ module.exports = {
 				.where('atmp_by').ne(studentId)
 				.skip(ranNum);
 
-			if (questionDocs === null) {
-				await interaction.update(
-					{ content: 'You\'re insane!! You answered every question at least once!!!', ephemeral: true, embeds: [], components: [] },
-				);
-				console.log('No more quiz questions.. ending quiz.');
-				return;
-			}
+			console.log(`Question text found: ${questionDocs.text}`);
+			console.log(`Total Attempts: ${questionDocs.tot_atmp}`);
 
 			console.log(`Skipped ${ranNum} and found question with Question ID: ${questionDocs._id}!`);
 			nextQuestion = questionDocs;
 
 			// Log Question in Student doc as Attempted
+			questionDocs.tot_atmp += 1;
+			console.log(`Total Attempts: ${questionDocs.tot_atmp}`);
 			questionDocs.atmp_by = studentId;
 			docSave(questionDocs);
 		} else {
@@ -107,7 +123,7 @@ module.exports = {
 
 		const attemptId = atmp._id;
 
-		console.log(`Creating new Attempt Doc: ${atmp}`);
+		console.log('Creating new Attempt Doc');
 		docSave(atmp);
 
 		// 4. send question to student
@@ -124,10 +140,25 @@ module.exports = {
 			.findOne()
 			.where('qs', nextQuestion._id)
 			.where('student', studentId)
-			.sort({ ['createdAt:']: -1 });
+			.sort({ createdAt: -1 });
 		console.log(`Last Attempt found: ${lastAtmp}`);
 
-		const efPrev = await lastEf(lastAtmp);
+		let lastWstreak = 0;
+		let lastLstreak = 0;
+		let lastEf = 2.5;
+		if (!lastAtmp) {
+			console.log(`lastAtmp: ${lastAtmp}`);
+		} else {
+			lastWstreak = lastAtmp.wstreak;
+			lastLstreak = lastAtmp.lstreak;
+			lastEf = lastAtmp.ef;
+			console.log(`lastAtmp wstreak: ${lastWstreak}`);
+			console.log(`lastAtmp lstreak: ${lastLstreak}`);
+			console.log(`lastAtmp ef: ${lastEf}`);
+
+			lastAtmp.r_atmp = true;
+			docSave(lastAtmp);
+		};
 
 		const newAtmp = await Attempt.findById(attemptId);
 
@@ -141,18 +172,26 @@ module.exports = {
 			if (i.customId === correctResponse) {
 				console.log(`✅ Correct response logged! ⌚ Time: ${seconds}`);
 				const ease = easinessCalc(true, seconds);
-				const newEF = efCalc(efPrev, ease);
-				const newWstreak = newAtmp.wstreak += 1;
+				console.log(`Ease calc: ${ease}`);
+				console.log(`lastEf: ${lastEf}`);
+				const newEF = efCalc(lastEf, ease);
+				console.log(`newEF: ${newEF}`);
+				console.log(`Old Lose Streak${lastLstreak}`);
+				const newLstreak = 0;
+				console.log(`New Lose Streak${newLstreak}`);
+				console.log(`Old Winstreak${lastWstreak}`);
+				const newWstreak = lastWstreak + 1;
+				console.log(`New Winstreak${newWstreak}`);
 				const newInt = daysToNext(newWstreak, newEF);
-				const nextDate = newDate(today, newInt);
-
+				const nextDate = newDate(startTime, newInt);
+				console.log(`Next Date is: ${nextDate}`);
 
 				newAtmp.ans = true;
 				newAtmp.ans_sec = seconds;
 				newAtmp.ease = ease;
 
 				newAtmp.wstreak = newWstreak;
-				newAtmp.lstreak = 0;
+				newAtmp.lstreak = newLstreak;
 				newAtmp.ef = newEF;
 				newAtmp.interval = newInt;
 				newAtmp.next_up = nextDate;
@@ -163,14 +202,25 @@ module.exports = {
 				i.update(
 					{ content: `Answer ${i.customId.toUpperCase()} recorded.`, embeds: [], components: [contRow] },
 				);
+			} else if (i.customId === 'sa-practice-end') {
+				i.update(
+					{ content: 'Thanks for Practicing!', ephemeral: true, embeds: [], components: [] },
+				);
 			} else {
 				console.log(`⛔ Oh no! Incorrect!! ⌚ Time: ${seconds}`);
 				const ease = easinessCalc(false, seconds);
-				const newEF = efCalc(efPrev, ease);
-				const newLstreak = newAtmp.lstreak += 1;
+				console.log(`Ease calc: ${ease}`);
+				console.log(`lastEf: ${lastEf}`);
+				const newEF = efCalc(lastEf, ease);
+				console.log(`Old Lose Streak${lastLstreak}`);
+				const newLstreak = lastLstreak + 1;
+				console.log(`New Lose Streak${newLstreak}`);
+				console.log(`Old Winstreak${lastWstreak}`);
 				const newWstreak = 0;
+				console.log(`New Winstreak${newWstreak}`);
 				const newInt = daysToNext(newWstreak, newEF);
-				const nextDate = newDate(today, newInt);
+				const nextDate = newDate(startTime, newInt);
+				console.log(`Next Date is: ${nextDate}`);
 
 				newAtmp.ans = false;
 				newAtmp.ans_sec = seconds;
@@ -198,6 +248,6 @@ module.exports = {
 
 		await interaction.update(
 			{ content: ' ', ephemeral: true, embeds: [embed], components: [ansRow] },
-		);
+		).catch(console.error);
 	},
 };
