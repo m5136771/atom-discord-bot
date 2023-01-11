5f2706c21beb9e62fa972d994a05f2765fe6d1d3
Michael DiPaolo <midipaolo@thesalisburyschool.org>
2023-01-11 09:08:49 -0500

    Add buttons & actionRows in separate file

--- assets/action-rows.js
+++ assets/action-rows.js
@@ -1,5 +1,11 @@
 const { ActionRowBuilder } = require('discord.js');
-const { ans1, ans2, ans3, ans4, endButton, nextButton } = require('../assets/buttons');
+const {
+	ans1, ans2,	ans3, ans4,
+	endButton, nextButton,
+	agileButton, apcspButton, blenderButton,
+	hardwareButton, htmlButton,
+	pythonButton, unityButton,
+} = require('../assets/buttons');
 
 const ansRow = new ActionRowBuilder()
 	.addComponents(ans1, ans2, ans3, ans4);
@@ -7,4 +13,15 @@ const ansRow = new ActionRowBuilder()
 const contRow = new ActionRowBuilder()
 	.addComponents(nextButton, endButton);
 
-module.exports = { ansRow, contRow };
+const SAOptionsRow1 = new ActionRowBuilder()
+	.addComponents(agileButton, apcspButton, blenderButton);
+
+const SAOptionsRow2 = new ActionRowBuilder()
+	.addComponents(hardwareButton, htmlButton, pythonButton, unityButton);
+
+module.exports = {
+	ansRow,
+	contRow,
+	SAOptionsRow1,
+	SAOptionsRow2,
+};
--- assets/buttons.js
+++ assets/buttons.js
@@ -1,5 +1,49 @@
 const { ButtonBuilder, ButtonStyle } = require('discord.js');
 
+// Skill Assessment Options
+const agileButton = new ButtonBuilder()
+	.setCustomId('agile-next')
+	.setLabel('Agile Scrum')
+	.setDisabled(true)
+	.setStyle(ButtonStyle.Primary);
+
+const apcspButton = new ButtonBuilder()
+	.setCustomId('apcsp-next')
+	.setLabel('AP CSP')
+	.setDisabled(true)
+	.setStyle(ButtonStyle.Primary);
+
+const blenderButton = new ButtonBuilder()
+	.setCustomId('blender-next')
+	.setLabel('Blender 3D')
+	.setDisabled(true)
+	.setStyle(ButtonStyle.Primary);
+
+const hardwareButton = new ButtonBuilder()
+	.setCustomId('hardware-next')
+	.setLabel('Hardware')
+	.setDisabled(true)
+	.setStyle(ButtonStyle.Primary);
+
+const htmlButton = new ButtonBuilder()
+	.setCustomId('html-next')
+	.setLabel('HTML')
+	.setDisabled(false)
+	.setStyle(ButtonStyle.Primary);
+
+const pythonButton = new ButtonBuilder()
+	.setCustomId('python-next')
+	.setLabel('Python')
+	.setDisabled(true)
+	.setStyle(ButtonStyle.Primary);
+
+const unityButton = new ButtonBuilder()
+	.setCustomId('unity-next')
+	.setLabel('Unity')
+	.setDisabled(true)
+	.setStyle(ButtonStyle.Primary);
+
+// Answer Options
 const ans1 = new ButtonBuilder()
 	.setCustomId('a')
 	.setLabel('A')
@@ -20,9 +64,10 @@ const ans4 = new ButtonBuilder()
 	.setLabel('D')
 	.setStyle(ButtonStyle.Primary);
 
+// Skill Assessment Continuation Options
 const endButton = new ButtonBuilder()
-	.setCustomId('quiz-end')
-	.setLabel('Submit & End Quiz')
+	.setCustomId('sa-practice-end')
+	.setLabel('Submit & End')
 	.setStyle(ButtonStyle.Success);
 
 const nextButton = new ButtonBuilder()
@@ -30,4 +75,10 @@ const nextButton = new ButtonBuilder()
 	.setLabel('Next Question')
 	.setStyle(ButtonStyle.Primary);
 
-module.exports = { ans1, ans2, ans3, ans4, endButton, nextButton };
+module.exports = {
+	ans1, ans2,	ans3, ans4,
+	endButton, nextButton,
+	agileButton, apcspButton, blenderButton,
+	hardwareButton, htmlButton,
+	pythonButton, unityButton,
+};
