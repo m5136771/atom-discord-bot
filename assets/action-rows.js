e8fdcfe5db7a0b208ae6b67c0538f27e25132bbd
Michael DiPaolo <midipaolo@thesalisburyschool.org>
2023-01-26 22:42:21 -0500

    Add buttons for all new commands

--- assets/action-rows.js
+++ assets/action-rows.js
@@ -1,40 +1,99 @@
 const { ActionRowBuilder } = require('discord.js');
 const {
 	ans1, ans2,	ans3, ans4,
-	endButton,
-	agileNextButton, apcspNextButton, htmlNextButton, pythonNextButton,
-	agileButton, apcspButton, blenderButton,
-	hardwareButton, htmlButton,
-	pythonButton, unityButton,
+
+	endButton, endGradedButton,
+
+	agileButton, agileGradedButton, agileNextButton, agileGradedNextButton,
+	apcspButton, apcspGradedButton, apcspNextButton, apcspGradedNextButton,
+	blenderButton, blenderGradedButton, blenderNextButton, blenderGradedNextButton,
+	hardwareButton, hardwareGradedButton, hardwareNextButton, hardwareGradedNextButton,
+	htmlButton, htmlGradedButton, htmlNextButton, htmlGradedNextButton,
+	pythonButton, pythonGradedButton, pythonNextButton, pythonGradedNextButton,
+	unityButton, unityGradedButton, unityNextButton, unityGradedNextButton,
 } = require('../assets/buttons');
 
 const ansRow = new ActionRowBuilder()
 	.addComponents(ans1, ans2, ans3, ans4);
 
+
+// Skills
 const agileContRow = new ActionRowBuilder()
 	.addComponents(agileNextButton, endButton);
+
+const agileGradedContRow = new ActionRowBuilder()
+	.addComponents(agileGradedNextButton, endGradedButton);
 
 const apcspContRow = new ActionRowBuilder()
 	.addComponents(apcspNextButton, endButton);
 
+const apcspGradedContRow = new ActionRowBuilder()
+	.addComponents(apcspGradedNextButton, endGradedButton);
+
+const blenderContRow = new ActionRowBuilder()
+	.addComponents(blenderNextButton, endButton);
+
+const blenderGradedContRow = new ActionRowBuilder()
+	.addComponents(blenderGradedNextButton, endGradedButton);
+
+const hardwareContRow = new ActionRowBuilder()
+	.addComponents(hardwareNextButton, endButton);
+
+const hardwareGradedContRow = new ActionRowBuilder()
+	.addComponents(hardwareGradedNextButton, endGradedButton);
+
 const htmlContRow = new ActionRowBuilder()
 	.addComponents(htmlNextButton, endButton);
+
+const htmlGradedContRow = new ActionRowBuilder()
+	.addComponents(htmlGradedNextButton, endGradedButton);
 
 const pythonContRow = new ActionRowBuilder()
 	.addComponents(pythonNextButton, endButton);
 
+const pythonGradedContRow = new ActionRowBuilder()
+	.addComponents(pythonGradedNextButton, endGradedButton);
+
+const unityContRow = new ActionRowBuilder()
+	.addComponents(unityNextButton, endButton);
+
+const unityGradedContRow = new ActionRowBuilder()
+	.addComponents(unityGradedNextButton, endGradedButton);
+
+
+// Continuation Rows
 const SAOptionsRow1 = new ActionRowBuilder()
 	.addComponents(agileButton, apcspButton, blenderButton);
 
 const SAOptionsRow2 = new ActionRowBuilder()
 	.addComponents(hardwareButton, htmlButton, pythonButton, unityButton);
 
+const GradedSAOptionsRow1 = new ActionRowBuilder()
+	.addComponents(agileGradedButton, apcspGradedButton, blenderGradedButton);
+
+const GradedSAOptionsRow2 = new ActionRowBuilder()
+	.addComponents(hardwareGradedButton, htmlGradedButton, pythonGradedButton, unityGradedButton);
+
 module.exports = {
 	ansRow,
+
 	agileContRow,
+	agileGradedContRow,
 	apcspContRow,
+	apcspGradedContRow,
+	blenderContRow,
+	blenderGradedContRow,
+	hardwareContRow,
+	hardwareGradedContRow,
 	htmlContRow,
+	htmlGradedContRow,
 	pythonContRow,
+	pythonGradedContRow,
+	unityContRow,
+	unityGradedContRow,
+
 	SAOptionsRow1,
 	SAOptionsRow2,
+	GradedSAOptionsRow1,
+	GradedSAOptionsRow2,
 };
--- assets/buttons.js
+++ assets/buttons.js
@@ -1,6 +1,6 @@
 const { ButtonBuilder, ButtonStyle } = require('discord.js');
 
-// Skill Assessment Options
+// Agile
 const agileButton = new ButtonBuilder()
 	.setCustomId('agile-next')
 	.setLabel('Agile Scrum')
@@ -13,6 +13,20 @@ const agileNextButton = new ButtonBuilder()
 	.setDisabled(false)
 	.setStyle(ButtonStyle.Primary);
 
+const agileGradedButton = new ButtonBuilder()
+	.setCustomId('agile-graded')
+	.setLabel('Agile Scrum')
+	.setDisabled(false)
+	.setStyle(ButtonStyle.Primary);
+
+const agileGradedNextButton = new ButtonBuilder()
+	.setCustomId('agile-graded')
+	.setLabel('Next Question')
+	.setDisabled(false)
+	.setStyle(ButtonStyle.Primary);
+
+
+// AP CSP
 const apcspButton = new ButtonBuilder()
 	.setCustomId('apcsp-next')
 	.setLabel('AP CSP')
@@ -24,18 +38,71 @@ const apcspNextButton = new ButtonBuilder()
 	.setLabel('Next Question')
 	.setStyle(ButtonStyle.Primary);
 
+const apcspGradedButton = new ButtonBuilder()
+	.setCustomId('apcsp-graded')
+	.setLabel('AP CSP')
+	.setDisabled(true)
+	.setStyle(ButtonStyle.Primary);
+
+const apcspGradedNextButton = new ButtonBuilder()
+	.setCustomId('apcsp-graded')
+	.setLabel('Next Question')
+	.setStyle(ButtonStyle.Primary);
+
+
+// Blender
 const blenderButton = new ButtonBuilder()
 	.setCustomId('blender-next')
 	.setLabel('Blender 3D')
 	.setDisabled(true)
 	.setStyle(ButtonStyle.Primary);
 
+const blenderNextButton = new ButtonBuilder()
+	.setCustomId('blender-next')
+	.setLabel('Blender 3D')
+	.setDisabled(true)
+	.setStyle(ButtonStyle.Primary);
+
+const blenderGradedButton = new ButtonBuilder()
+	.setCustomId('blender-graded')
+	.setLabel('Blender 3D')
+	.setDisabled(true)
+	.setStyle(ButtonStyle.Primary);
+
+const blenderGradedNextButton = new ButtonBuilder()
+	.setCustomId('blender-graded')
+	.setLabel('Blender 3D')
+	.setDisabled(true)
+	.setStyle(ButtonStyle.Primary);
+
+
+// Hardware
 const hardwareButton = new ButtonBuilder()
 	.setCustomId('hardware-next')
 	.setLabel('Hardware')
 	.setDisabled(true)
 	.setStyle(ButtonStyle.Primary);
 
+const hardwareNextButton = new ButtonBuilder()
+	.setCustomId('hardware-next')
+	.setLabel('Hardware')
+	.setDisabled(true)
+	.setStyle(ButtonStyle.Primary);
+
+const hardwareGradedButton = new ButtonBuilder()
+	.setCustomId('hardware-graded')
+	.setLabel('Hardware')
+	.setDisabled(true)
+	.setStyle(ButtonStyle.Primary);
+
+const hardwareGradedNextButton = new ButtonBuilder()
+	.setCustomId('hardware-graded')
+	.setLabel('Hardware')
+	.setDisabled(true)
+	.setStyle(ButtonStyle.Primary);
+
+
+// HTML
 const htmlButton = new ButtonBuilder()
 	.setCustomId('html-next')
 	.setLabel('HTML')
@@ -48,6 +115,20 @@ const htmlNextButton = new ButtonBuilder()
 	.setDisabled(false)
 	.setStyle(ButtonStyle.Primary);
 
+const htmlGradedButton = new ButtonBuilder()
+	.setCustomId('html-graded')
+	.setLabel('HTML')
+	.setDisabled(false)
+	.setStyle(ButtonStyle.Primary);
+
+const htmlGradedNextButton = new ButtonBuilder()
+	.setCustomId('html-graded')
+	.setLabel('Next Question')
+	.setDisabled(false)
+	.setStyle(ButtonStyle.Primary);
+
+
+// Python
 const pythonButton = new ButtonBuilder()
 	.setCustomId('python-next')
 	.setLabel('Python')
@@ -59,11 +140,43 @@ const pythonNextButton = new ButtonBuilder()
 	.setLabel('Next Question')
 	.setStyle(ButtonStyle.Primary);
 
+const pythonGradedButton = new ButtonBuilder()
+	.setCustomId('python-graded')
+	.setLabel('Python')
+	.setDisabled(false)
+	.setStyle(ButtonStyle.Primary);
+
+const pythonGradedNextButton = new ButtonBuilder()
+	.setCustomId('python-graded')
+	.setLabel('Next Question')
+	.setStyle(ButtonStyle.Primary);
+
+
+// Unity
 const unityButton = new ButtonBuilder()
 	.setCustomId('unity-next')
 	.setLabel('Unity')
 	.setDisabled(true)
 	.setStyle(ButtonStyle.Primary);
+
+const unityNextButton = new ButtonBuilder()
+	.setCustomId('unity-next')
+	.setLabel('Unity')
+	.setDisabled(true)
+	.setStyle(ButtonStyle.Primary);
+
+const unityGradedButton = new ButtonBuilder()
+	.setCustomId('unity-graded')
+	.setLabel('Unity')
+	.setDisabled(true)
+	.setStyle(ButtonStyle.Primary);
+
+const unityGradedNextButton = new ButtonBuilder()
+	.setCustomId('unity-graded')
+	.setLabel('Unity')
+	.setDisabled(true)
+	.setStyle(ButtonStyle.Primary);
+
 
 // Answer Options
 const ans1 = new ButtonBuilder()
@@ -86,17 +199,28 @@ const ans4 = new ButtonBuilder()
 	.setLabel('D')
 	.setStyle(ButtonStyle.Primary);
 
+
 // Skill Assessment Continuation Options
 const endButton = new ButtonBuilder()
 	.setCustomId('practice-end')
 	.setLabel('Submit & End')
 	.setStyle(ButtonStyle.Success);
 
+const endGradedButton = new ButtonBuilder()
+	.setCustomId('graded-end')
+	.setLabel('Submit & End')
+	.setStyle(ButtonStyle.Success);
+
 module.exports = {
 	ans1, ans2,	ans3, ans4,
-	endButton,
-	agileNextButton, apcspNextButton, htmlNextButton, pythonNextButton,
-	agileButton, apcspButton, blenderButton,
-	hardwareButton, htmlButton,
-	pythonButton, unityButton,
+
+	endButton, endGradedButton,
+
+	agileButton, agileGradedButton, agileNextButton, agileGradedNextButton,
+	apcspButton, apcspGradedButton, apcspNextButton, apcspGradedNextButton,
+	blenderButton, blenderGradedButton, blenderNextButton, blenderGradedNextButton,
+	hardwareButton, hardwareGradedButton, hardwareNextButton, hardwareGradedNextButton,
+	htmlButton, htmlGradedButton, htmlNextButton, htmlGradedNextButton,
+	pythonButton, pythonGradedButton, pythonNextButton, pythonGradedNextButton,
+	unityButton, unityGradedButton, unityNextButton, unityGradedNextButton,
 };
